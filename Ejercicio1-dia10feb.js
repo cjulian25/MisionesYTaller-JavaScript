9b12118b97a173f68c6a8ed9e46d9c429d0765e2
cjulian25 <carrillojulian@hotmail.com>
2023-02-10 15:36:51 -0500

    Finalizado

--- Ejercicio1-dia10feb.js
+++ Ejercicio1-dia10feb.js
@@ -7,12 +7,12 @@ console.log(texto)
 console.log(num1+num2)
 
 if(num1 === "3"){
-console.log("obviusly")
+  console.log("obviusly")
 }else if(num1 === 3){
-console.log("ay caramba")
+  console.log("ay caramba")
 }else{
-console.log("obviusly not")
-}
+  console.log("obviusly not")
+  }
 
 let edad = parseInt(prompt("Digita tu edad: "))
 console.log(edad) */
@@ -25,34 +25,37 @@ let num2 = parseInt(prompt("Digita el segundo numero"))
 let num3 = parseInt(prompt("Digita el tercer numero"))
 
 if(num1 == num2 | num1 == num3 | num2 == num3){
-console.log("Digitaste dos o mas numeros iguales")
-}else if(num1 > num2 & num1 > num3){
-console.log(`El numero mayor es: ${num1}`)
-}else if(num2 > num1 & num2 > num3){
-console.log(`El numero mayor es: ${num2}`)
-}else if(num3 > num1 & num3 > num2){
-console.log(`El numero mayor es: ${num3}`)
-}
+  console.log("Digitaste dos o mas numeros iguales")
+  } else if(num1 > num2 & num1 > num3){
+  console.log(`El numero mayor es: ${num1}`)
+  } else if(num2 > num1 & num2 > num3){
+  console.log(`El numero mayor es: ${num2}`)
+  } else if(num3 > num1 & num3 > num2){
+  console.log(`El numero mayor es: ${num3}`)
+  }
 if(num1 < num2 & num1 < num3){
-console.log(`El numero menor es: ${num1}`)
-}else if(num2 < num1 & num2 < num3){
-console.log(`El numero menor es: ${num2}`)
-}else if(num3 < num1 & num3 < num2){
-console.log(`El numero menor es: ${num3}`) */
+  console.log(`El numero menor es: ${num1}`)
+  } else if(num2 < num1 & num2 < num3){
+  console.log(`El numero menor es: ${num2}`)
+  } else if(num3 < num1 & num3 < num2){
+  console.log(`El numero menor es: ${num3}`)
+ } */
 
-/* 2. Desarrolle un algoritmo que reciba la base y altura de un triangulo en cm. Los 2 valores ingresados no pueden ser iguales, ademas el algoritmo debe calcular el area del triangulo si la base es mayor a 5cm y la altura es mayor a 4cm, si no lo son el algoritmo debe imprimir el valor de la base multiplicado por 2 y sumarle 10. */
+/* 2. Desarrolle un algoritmo que reciba la base y altura de un triangulo en cm. Los 2 valores ingresados no pueden ser iguales, ademas el algoritmo debe 
+calcular el area del triangulo si la base es mayor a 5cm y la altura es mayor a 4cm, si no lo son el algoritmo debe imprimir el valor de la base 
+multiplicado por 2 y sumarle 10. */
 
 /* let base = parseFloat(prompt("Digita la base del triangulo"))
 let altura = parseFloat(prompt("Digita la altura del triangulo"))
 
 while(base == altura){
-altura = parseFloat(prompt("Los numeros no pueden ser iguales, vuelve a digitar la altura del triangulo"))
-}
+  altura = parseFloat(prompt("Los numeros no pueden ser iguales, vuelve a digitar la altura del triangulo"))
+  }
 if(base > 5 & altura > 4){
   console.log(`El area del triangulo es: ${(base*altura)/2}`)
-}else{
+  } else{
   console.log(`${(base*2)+10}`)
-}  */
+  }  */
 
 /* 3. Desarrolle un algoritmo que reciba un numero entero y escriba si dicho numero es par o impar */
 
@@ -60,9 +63,9 @@ if(base > 5 & altura > 4){
 
 if(num%2 == 0){
 	console.log(`El numero ${num} es par`)
-}else{
+  } else{
 	console.log(`El numero ${num} es impar`)
-} */
+  } */
 
 /* 4. Desarrolle un algoritmo que permita convertir calificaciones numericas, segun la siguiente tabla:
 A = 19 y 20
@@ -76,20 +79,20 @@ Las notas solo pueden  ser numeros entre 1 y 20 */
 let letra = ""
 
 while(num<1 | num>20){
-num = parseInt(prompt("Digita un numero entre 1 y 20"));
+  num = parseInt(prompt("Digita un numero entre 1 y 20"));
 }
 
 if(num==19 & num==20){
-   letra = "A"
-} else if(num>=16 & num<=18){
-  letra = "B"
-}else if(num>=13 & num<=15){
-  letra = "C"
-}else if(num>=10 & num<=12){
-  letra = "D"
-}else{
-  letra = "E"
-}
+  letra = "A"
+  } else if(num>=16 & num<=18){
+    letra = "B"
+  } else if(num>=13 & num<=15){
+    letra = "C"
+  } else if(num>=10 & num<=12){
+    letra = "D"
+  } else{
+    letra = "E"
+  }
 console.log(letra) */
 
 /* 5. Pedir dos numeros y decir si son multiplos o no. */
@@ -128,5 +131,23 @@ if(num==0){
 }
 console.log(texto)
  */
- 
-/*  7.  */
+
+/*7.  Pedir tres numeros enteros con un valor del 1 al 10, sacar el promedio de los tres numeros y mostrar
+True si el promedio es mayor a 5 en caso contrario mostrar Flase, utilizar el operador ternario.*/
+
+/*let sumatoria = 0
+let promedio = 0
+
+for(var i = 0; i < 3; i++){
+  let num = parseInt(prompt(`Digita el ${i+1}° numero`))
+  while(num < 1 | num > 10){
+    num = parseInt(prompt(`El ${i+1}° numero debe estar entre un rango del 1 al 10`))
+  }
+  sumatoria += num
+  promedio = sumatoria/3
+}
+
+promedio>5 ? console.log(`True`) : console.log(`False`) */
+
+/*8. Desarrolle un algoritma que reciba un numero entero e imprima si el numero es par o impar. 
+Ademas el numero debe ser positivo*/
